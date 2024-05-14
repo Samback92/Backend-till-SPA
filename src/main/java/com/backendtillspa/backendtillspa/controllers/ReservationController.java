c0f9cf253ba9365a3aadb5eb280e41bf0ddd1701
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-05-14 10:04:34 +0200

    Update ReservationController.java

--- src/main/java/com/backendtillspa/backendtillspa/controllers/ReservationController.java
+++ src/main/java/com/backendtillspa/backendtillspa/controllers/ReservationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://king-prawn-app-44rdd.ondigitalocean.app/")
 public class ReservationController {
     
     private ReservationService reservationService;
